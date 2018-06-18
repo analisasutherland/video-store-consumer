c73ad84c3be50939a496b2329085cebd741e07e2
Kiera <kiera.thomasson@gmail.com>
2018-06-18 15:27:32 -0700

    created movie and videostore components. created input of form to search a movie.

---
+++ src/components/VideoStore.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import axios from 'axios';
+
+import Movie from './Movie';
+
+class VideoStore extends Component {
+  render(){
+    return(
+      <div>
+        <Movie />
+      </div>
+    );
+  }
+
+}
+
+
+export default VideoStore;
