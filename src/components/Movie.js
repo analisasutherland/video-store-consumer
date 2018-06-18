2f3fe20f327f26fcbc07b222d75df91fbe5a7da9
Kiera <kiera.thomasson@gmail.com>
2018-06-18 16:02:24 -0700

    build out movie search form. Added onInputChange and onFormSubmit functions.

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -2,14 +2,49 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Movie extends Component {
+  static propTypes = {
+    searchMovieCallback: PropTypes.func.isRequired
+  }
+
+  constructor() {
+    super();
+    this.state = {
+      query: ""
+    };
+  }
+
+  onInputChange = (event) => {
+    const key = event.target.name;
+    let value = event.target.value;
+
+    let updatedInput = {};
+    updatedInput[key] = value;
+
+    console.log(this.state);
+    this.setState(updatedInput);
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log("form submit");
+    console.log(this.state);
+
+    this.props.searchMovieCallback(this.state);
+
+    this.setState({query: ""})
+  }
+
   render(){
     return(
       <div>
-        <form className='movie-search-bar'>
+        <form onSubmit={this.onFormSubmit} className='movie-search-bar'>
           <input
             type="text"
             name="query"
-            placeholder="enter search word"  />
+            placeholder="enter search word"
+            onChange={this.onInputChange}
+            value={this.state.query}
+           />
             <input type="submit"/>
         </form>
         <div className='movie-library-link'>
